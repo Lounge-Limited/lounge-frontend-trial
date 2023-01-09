bd640b354b3b5c62f3b6031885426f23b9d57db5
Mohamed Abdelmagid <mohammedeihabmail+github@gmail.com>
2023-01-09 18:17:29 +0000

    Basic trial app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,123 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { PostsData, UsersData } from './data';
+
+
+const Post = ({
+  title,
+  content,
+  author,
+  avatar,
+}: {
+  title: string;
+  content: string;
+  author: string;
+  avatar: string;
+}) => {
+  return (
+    <div
+      style={{
+        border: '1px solid black',
+        padding: '10px',
+        margin: '10px',
+        backgroundColor: 'white',
+      }}
+    >
+      <h1
+        style={{
+          fontSize: '20px',
+          margin: '0',
+        }}
+      >{title}</h1>
+      <p
+        style={{
+          fontSize: '14px',
+          margin: '0',
+          padding: '10px 0',
+        }}
+      >{content}</p>
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <img
+
+            style={{
+              width: '20px',
+              height: '20px',
+              borderRadius: '50%',
+              marginRight: '5px',
+            }}
+            src={avatar}
+            alt="avatar"
+          />
+          <p
+            style={{
+              fontSize: '12px',
+              margin: '0',
+            }}
+          >{author}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (      
+  <div
+    style={{
+      margin: "auto",
+      width: "80%",
+      padding: "10px",
+      backgroundColor: "#f0f0f0",
+    }}
+  >
+    <h1>News Feed</h1>
+      <Post
+      title={PostsData[0].title}
+      content={PostsData[0].content}
+      author={UsersData[0].name}
+      avatar={UsersData[0].avatar_url}
+      />
+    <Post
+      title={PostsData[1].title}
+      content={PostsData[1].content}
+      author={UsersData[1].name}
+      avatar={UsersData[1].avatar_url}
+      />
+    <Post
+      title={PostsData[2].title}
+      content={PostsData[2].content}
+      author={UsersData[2].name}
+      avatar={UsersData[2].avatar_url}
+      />
+    <Post
+      title={PostsData[3].title}
+      content={PostsData[3].content}
+      author={UsersData[3].name}
+      avatar={UsersData[3].avatar_url}
+      />
+          <Post
+      title={PostsData[4].title}
+      content={PostsData[4].content}
+      author={UsersData[4].name}
+      avatar={UsersData[4].avatar_url}
+      />
+  </div>
   );
 }
+
 
 export default App;
---
+++ src/api.ts
@@ -0,0 +1,39 @@
+
+const API_URL = "http://localhost:3004"
+
+export function getPostById(id: string) {
+  return fetch(`${API_URL}/posts/${id}`).then((res) => res.json());
+}
+
+// export function getPosts() {
+//   return fetch(`${API_URL}/posts`).then((res) => res.json());
+// }
+
+export function getUserById(id: string) {
+  return fetch(`${API_URL}/users/${id}`).then((res) => res.json());
+}
+
+export function getUsers() {
+  return fetch(`${API_URL}/users`).then((res) => res.json());
+}
+
+
+export function getPosts() {
+  return new Promise((resolve, reject) => {
+    const random = Math.random();
+
+    fetch(`${API_URL}/posts`).then((res) => {
+      if (random < 0.1) {
+        reject("Failed to fetch posts");
+      } else if (random < 0.3) {
+        setTimeout(() => {
+          resolve(res.json());
+        }, 3000);
+      } else {
+        resolve(res.json());
+      }
+    })
+  });
+}
+
+
